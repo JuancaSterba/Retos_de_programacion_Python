ea4e2dcd1f4375c75141fb050075325f75710bcd
JuancaSterba <sjcexe@gmail.com>
2023-07-30 19:31:41 -0300

    segundo reto

--- 00_retos_inicial.py
+++ 00_retos_inicial.py
@@ -20,3 +20,22 @@ def fizz_buzz():
             print(i)
 
 fizz_buzz()
+
+"""
+Reto #1: ¿ES UN ANAGRAMA?
+Escribe una función que reciba dos palabras (String) y retorne
+verdadero o falso (Bool) según sean o no anagramas.
+- Un Anagrama consiste en formar una palabra reordenando TODAS
+  las letras de otra palabra inicial.
+- NO hace falta comprobar que ambas palabras existan.
+- Dos palabras exactamente iguales no son anagrama.
+"""
+
+def anagrama(palabra1, palabra2):
+    if sorted(palabra1.lower()) == sorted(palabra2.lower()):
+        return True
+    else:
+        return False
+
+resultado = anagrama("ramon","ramen")
+print(resultado)
