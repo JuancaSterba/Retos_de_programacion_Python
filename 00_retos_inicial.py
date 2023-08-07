b94bb2db200531853293be124c2f2c8743491a42
JuancaSterba <sjcexe@gmail.com>
2023-08-07 12:38:24 -0300

    Se realizar el reto #4

--- 00_retos_inicial.py
+++ 00_retos_inicial.py
@@ -90,3 +90,33 @@ def primo(numero):
 for i in range(1, 101):
     if primo(i):
         print(i)
+
+"""
+Reto #4: ÁREA DE UN POLÍGONO
+Crea una única función (importante que sólo sea una) que sea capaz
+de calcular y retornar el área de un polígono.
+- La función recibirá por parámetro sólo UN polígono a la vez.
+- Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+- Imprime el cálculo del área de un polígono de cada tipo.
+"""
+
+def area(*lados):
+    if len(lados) == 1:
+        lado = lados[0]
+        area = lado ** 2
+        print(f"El area del cuadrado es: {area}")
+    elif len(lados) == 2:
+        lado = lados[0]
+        lado2 = lados[1]
+        area = lado * lado2
+        print(f"El area del rectángulo es: {area}")
+    elif len(lados) == 3:
+        lado = lados[0]
+        lado2 = lados[1]
+        lado3 = lados[2]
+        area = (lado * lado2) / 2
+        print(f"El area del triángulo es: {area}")
+    else:
+        print("El polígono no es válido")
+
+area(3,4,5,6)
