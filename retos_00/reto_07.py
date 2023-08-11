9fa258d3a5f14ccdfb59f0c33fafaa4be31ca68f
JuancaSterba <sjcexe@gmail.com>
2023-08-11 14:07:09 -0300

    Se crea el reto # 07

---
+++ retos_00/reto_07.py
@@ -0,0 +1,37 @@
+"""
+Reto #7: CONTANDO PALABRAS
+
+Crea un programa que cuente cuantas veces se repite cada palabra
+y que muestre el recuento final de todas ellas.
+- Los signos de puntuación no forman parte de la palabra.
+- Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+- No se pueden utilizar funciones propias del lenguaje que
+  lo resuelvan automáticamente.
+
+"""
+
+"""def cuenta_palabras(texto):
+  palabras = texto.split()
+  dicc = {}
+  for palabra in palabras:
+      if palabra in dicc:
+          dicc[palabra] += 1
+      else:
+          dicc[palabra] = 1
+  return dicc"""
+  
+def cuenta_palabras(texto):
+  texto = texto.lower()
+  texto = texto.replace(".", "").replace(",", "").replace(":", "").replace(";","")
+  palabras = texto.split()
+  dicc = {}
+  for palabra in palabras:
+      if palabra in dicc:
+          dicc[palabra] += 1
+      else:
+          dicc[palabra] = 1
+  return dicc  
+
+texto = "Python es un lenguaje de programación de propósito general, de uso común, y de código abierto."
+
+print(cuenta_palabras(texto))
